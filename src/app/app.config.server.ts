6f58eb45c628c3ea72dcb9197bc0a42b0e67c8d4
truongpham1309 <truong13092002@gmail.com>
2024-02-23 14:17:20 +0700

    base FinnalTest

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,13 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
   ]
 };
 
--- src/app/components/pages/text/text.component.ts
+++ src/app/components/pages/text/text.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-text',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './text.component.html',
   styleUrl: './text.component.css'
 })
 export class TextComponent {
-
+  
 }
