8d54e630a0389b9365b7bb63b985bf991e2d0c0a
truongpham1309 <truong13092002@gmail.com>
2024-02-24 07:29:00 +0700

    -u base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LayoutAdminComponent } from './components/layouts/layout-admin/layout-admin.component';
 import { TextComponent } from './components/pages/text/text.component';
+import { NotfoundComponent } from './components/pages/notfound/notfound.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,10 @@ export const routes: Routes = [
             {
                 path:'',
                 component: TextComponent
+            },
+            {
+                path: "**",
+                component: NotfoundComponent,
             }
         ]
     }
