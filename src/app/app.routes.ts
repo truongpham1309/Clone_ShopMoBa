958092552bec042568dd705bd0325ae360815cf5
truongpham1309 <truong13092002@gmail.com>
2024-02-24 13:38:36 +0700

    -u base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LayoutAdminComponent } from './components/layouts/layout-admin/layout-admin.component';
 import { TextComponent } from './components/pages/text/text.component';
 import { NotfoundComponent } from './components/pages/notfound/notfound.component';
+import { adminGuardGuard } from './guard/admin-guard.guard';
 
 export const routes: Routes = [
     {
         path: "",
+        canActivate: [adminGuardGuard],
         component: LayoutAdminComponent,
         children: [
             {
